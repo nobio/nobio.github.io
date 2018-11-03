d79a032de6cde90e5bf91e8d945f6ac0634cff50
Gernot H. Reichel <gernot.reichel@me.com>
2018-11-03 09:38:00 +0100

    deploy

---
+++ js/author.c2de224a.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["author"],{9411:function(e,t,n){"use strict";n.r(t);var r=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},s=[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticClass:"author"},[n("h1",[e._v("This is the author's page (namely Schernoo)")])])}],a=n("2877"),o={},u=Object(a["a"])(o,r,s,!1,null,null,null);u.options.__file="Author.vue";t["default"]=u.exports}}]);
+//# sourceMappingURL=author.c2de224a.js.map
---
+++ precache-manifest.b585fa95db0a9844c550335e06e04532.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "ca407450bab8fbef02ec",
+    "url": "/js/chunk-vendors.a3d6e407.js"
+  },
+  {
+    "revision": "c5a4c1d4b3d57b3c62d3",
+    "url": "/js/author.c2de224a.js"
+  },
+  {
+    "revision": "fd7d2f15bcde32626c6e",
+    "url": "/js/app.09970f37.js"
+  },
+  {
+    "revision": "da1eaa38473577995c90",
+    "url": "/js/about.8a391d75.js"
+  },
+  {
+    "revision": "af1ae9f078bdd28580fed6e0398bbfea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "fd7d2f15bcde32626c6e",
+    "url": "/css/app.37cd8f8c.css"
+  }
+];
